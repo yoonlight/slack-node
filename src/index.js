811fe9b6801dcd5d9552e806070f706ff9dc112d
yoonlight <yoonlight12@gmail.com>
2021-03-26 15:58:02 +0900

    complete slack api apply to node

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
-import express, { Router } from 'express'
-import dotenv from 'dotenv'
-import schedule from 'node-schedule'
-import { WebClient } from '@slack/web-api'
-import api from './router/index.js'
+import dotenv from 'dotenv';
+// import schedule from 'node-schedule'
+import { WebClient } from '@slack/web-api';
+import amqplib from 'amqplib/callback_api';
 
-dotenv.config()
-const web = new WebClient(process.env.SLACK_TOKEN)
-const app = express()
-async () => {
-  try {
-    const list = await web.channels.list('general')
-    console.log(list)
-  } catch (error) {
-    console.log(error.data)
-  }
-}
+dotenv.config();
+const token = process.env.SLACK_TOKEN;
 
-app.use('/api', api)
-app.get('/', (req, res)=> {
-  res.send('hello world!')
-})
-app.listen(process.env.SERVER_PORT, () => {
-  console.log('server listening to', process.env.SERVER_PORT)
-})
-
+const web = new WebClient(token);
 // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
-const conversationId = '';
-
+const convoId = process.env.SLACK_CONVERSATION_ID;
+const text = 'Hello there';
 (async () => {
   try {
     // See: https://api.slack.com/methods/chat.postMessage
-    const res = await web.chat.postMessage({ channel: conversationId, text: 'Hello there' });
+    const res = await web.chat.postMessage({ channel: convoId, text: text });
 
     // `res` contains information about the posted message
     console.log('Message sent: ', res.ts);
   } catch (error) {
-      console.log(error.data);
+    console.log(error.data);
   }
-})()
+})();
+
+amqplib.connect(process.env.RABBITMQ_URL, (error0, connection) => {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel((error1, channel) => {
+    if (error1) {
+      throw error1;
+    }
+    const queue = 'slack';
+    channel.assertQueue(queue, { durable: false });
+    console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', queue);
+    channel.consume(queue, function (msg) {
+      console.log(' [x] Received %s', msg.content.toString());
+    });
+  });
+});
