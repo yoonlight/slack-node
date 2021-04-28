170a6b788a5152b25b24388e215f777245e690e6
yoonlight <yoonlight12@gmail.com>
2021-04-28 16:56:13 +0900

    code refactoring & use async-await

--- src/index.js
+++ src/index.js
@@ -1,45 +1,7 @@
-import dotenv from 'dotenv';
-import { WebClient } from '@slack/web-api';
-import amqplib from 'amqplib/callback_api';
+import { mqLoad } from './loader/mq';
 
-dotenv.config();
-const env = process.env;
-const token = env.SLACK_TOKEN;
-const convoId = env.SLACK_CONVERSATION_ID;
-const rabbitMQ_URL = env.RABBITMQ_URL;
-const web = new WebClient(token);
+async function load() {
+  await mqLoad();
+}
 
-amqplib.connect(rabbitMQ_URL, (error0, connection) => {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel((error1, channel) => {
-    if (error1) {
-      throw error1;
-    }
-    const queue = 'slack';
-    channel.assertQueue(queue, { durable: false });
-    console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', queue);
-    channel.consume(
-      queue,
-      async function (msg) {
-        console.log(' [x] Received %s', msg.content.toString());
-        try {
-          // See: https://api.slack.com/methods/chat.postMessage
-          const res = await web.chat.postMessage({
-            channel: convoId,
-            text: msg.content.toString(),
-          });
-
-          // `res` contains information about the posted message
-          console.log('Message sent: ', res.ts);
-        } catch (error) {
-          console.log(error.data);
-        }
-      },
-      {
-        noAck: true,
-      },
-    );
-  });
-});
+load();
---
+++ src/loader/mq.js
@@ -0,0 +1,46 @@
+import amqp from 'amqplib';
+import { WebClient } from '@slack/web-api';
+
+const env = process.env;
+const token = env.SLACK_TOKEN;
+const convoId = env.SLACK_CONVERSATION_ID;
+const rabbitMQ_URL = env.RABBITMQ_URL;
+const web = new WebClient(token);
+
+async function mqLoad() {
+  const q = 'slack';
+  const open = amqp.connect(rabbitMQ_URL);
+
+  try {
+    const conn = await open;
+    const ch = await conn.createChannel();
+    let ok = ch.assertQueue(q, { durable: false });
+    ok = ok.then(() => {
+      return ch.consume(q, async function (msg) {
+        if (msg !== null) {
+          console.log(' [x] Received %s', msg.content.toString());
+          try {
+            // See: https://api.slack.com/methods/chat.postMessage
+            const res = await web.chat.postMessage({
+              channel: convoId,
+              text: msg.content.toString(),
+            });
+
+            // `res` contains information about the posted message
+            console.log('Message sent: ', res.ts);
+          } catch (error) {
+            console.log(error.data);
+          }
+          ch.ack(msg);
+        }
+      });
+    });
+    return ok.then(() => {
+      console.log(' [*] Waiting for logs. To exit press CTRL+C');
+    });
+  } catch (error) {
+    console.warn(error);
+  }
+}
+
+export { mqLoad };
