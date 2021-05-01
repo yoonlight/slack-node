ee65e6044900f706b2c4488bf8c4e430609ae72d
kjw <yoonlight@gmail.com>
2021-05-01 23:09:29 +0900

    router

--- src/index.js
+++ src/index.js
@@ -8,9 +8,30 @@ expressLoader(app);
 app.listen(3002, () => {
   console.log('listen');
 });
-botkit(app);
+const controller = botkit(app);
 async function load() {
   await mqLoad();
 }
 
 load();
+
+app.post('/api/messages', function (req, res) {
+  // respond to FB that the webhook has been received.
+  res.status(200);
+  res.send('ok');
+
+  var bot = controller.spawn({});
+
+  // Now, pass the webhook into be processed
+  controller.handleWebhookPayload(req, res, bot);
+});
+// Perform the FB webhook verification handshake with your verify token
+app.get('/api/receive', function (req, res) {
+  if (req.query['hub.mode'] == 'subscribe') {
+    if (req.query['hub.verify_token'] == controller.config.verify_token) {
+      res.send(req.query['hub.challenge']);
+    } else {
+      res.send('OK');
+    }
+  }
+});
--- src/loader/botkit.js
+++ src/loader/botkit.js
@@ -12,4 +12,6 @@ export default (app) => {
     webserver: app,
     // ...other options
   });
+
+  return controller;
 };
