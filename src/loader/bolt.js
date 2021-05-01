0b04c81067d2be18313b5f71eb6cb4ed7850375d
kjw <yoonlight@gmail.com>
2021-05-02 00:23:58 +0900

    modify home opened

--- src/loader/bolt.js
+++ src/loader/bolt.js
@@ -37,9 +37,56 @@ app.action('button_click', async ({ ack, say }) => {
   await say('Request approved 👍');
 });
 
-app.event('app_home_opened', async ({ ack }) => {
-  ack();
-  console.log('Blah');
+app.event('app_home_opened', async ({ event, client, context }) => {
+  try {
+    /* view.publish is the method that your app uses to push a view to the Home tab */
+    const result = await client.views.publish({
+      /* the user that opened your app's app home */
+      user_id: event.user,
+
+      /* the view object that appears in the app home*/
+      view: {
+        type: 'home',
+        callback_id: 'home_view',
+
+        /* body of the view */
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: "*Welcome to your _App's Home_* :tada:",
+            },
+          },
+          {
+            type: 'divider',
+          },
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text:
+                "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app.",
+            },
+          },
+          {
+            type: 'actions',
+            elements: [
+              {
+                type: 'button',
+                text: {
+                  type: 'plain_text',
+                  text: 'Click me!',
+                },
+              },
+            ],
+          },
+        ],
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 export default async () => {
