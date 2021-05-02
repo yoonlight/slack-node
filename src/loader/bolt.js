65a9edf6132bfdb864003fc2152b1a6e9e2d6549
kjw <yoonlight@gmail.com>
2021-05-02 10:53:35 +0900

    add skill

--- src/loader/bolt.js
+++ src/loader/bolt.js
@@ -85,6 +85,70 @@ app.event('app_home_opened', async ({ event, client, context }) => {
         ],
       },
     });
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+const whenSeptemberEnds = '1569887999';
+
+app.message('wake me up', async ({ message, client }) => {
+  try {
+    // Call chat.scheduleMessage with the built-in client
+    const result = await client.chat.scheduleMessage({
+      channel: message.channel,
+      post_at: whenSeptemberEnds,
+      text: 'Summer has come and passed',
+    });
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.shortcut('open_modal', async ({ shortcut, ack, client }) => {
+  try {
+    // Acknowledge shortcut request
+    await ack();
+
+    // Call the views.open method using one of the built-in WebClients
+    const result = await client.views.open({
+      trigger_id: shortcut.trigger_id,
+      view: {
+        type: 'modal',
+        title: {
+          type: 'plain_text',
+          text: 'My App',
+        },
+        close: {
+          type: 'plain_text',
+          text: 'Close',
+        },
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text:
+                'About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>.',
+            },
+          },
+          {
+            type: 'context',
+            elements: [
+              {
+                type: 'mrkdwn',
+                text:
+                  'Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>',
+              },
+            ],
+          },
+        ],
+      },
+    });
+
+    console.log(result);
   } catch (error) {
     console.error(error);
   }
